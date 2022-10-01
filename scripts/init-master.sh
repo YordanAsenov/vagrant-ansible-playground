721c3ffebf73efac649271ead31a3f23250644ac
Yordan Asenov <yordanasenov@gmail.com>
2022-10-01 23:47:43 +0200

    upgrade ansible-core to latest version

--- scripts/init-master.sh
+++ scripts/init-master.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "Start init-master"
-apt-get install ansible-core -y
+apt install software-properties-common -y
+add-apt-repository --yes --update ppa:ansible/ansible  -y
+add-apt-repository ppa:ansible/ansible -y
+apt update -y
+apt install ansible-core -y
+apt autoremove -y
 
 mkdir -p /etc/ansible
 cp /home/vagrant/.ansible/ansible.cfg /etc/ansible/ansible.cfg
