0ac1a8f64b1edc9978da55970e3ba178b5f44e72
Yordan Asenov <yordanasenov@gmail.com>
2022-10-01 16:15:41 +0200

    upgrade ansible to ansible-core

--- scripts/init-master.sh
+++ scripts/init-master.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Start init-master"
-apt-get install ansible -y
+apt-get install ansible-core -y
+
 mkdir -p /etc/ansible
 cp /home/vagrant/.ansible/ansible.cfg /etc/ansible/ansible.cfg
 apt-get install sshpass -y
